9d6d3dc2d66159b04557ff414935b9a471c46a18
glyyynn <glynnjordan.jr@gmail.com>
2024-09-07 19:40:59 -0500

    testing session user change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { HeroForm } from '@/components/form';
 import { Icons } from '@/components/icons';
 import { Button } from '@/components/ui/button';
+import { Greeting } from '@/components/greeting';
 import * as m from '@/paraglide/messages';
 
 const Home = () => {
   return (
     <section className="container mt-10 flex flex-col items-center gap-3 text-center md:absolute md:left-1/2 md:top-1/2 md:mt-0 md:-translate-x-1/2 md:-translate-y-1/2">
+      <h1 className="mb-1 font-mono text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+        <Greeting />
+      </h1>
       <h1 className="mb-1 font-mono text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
         {m.main_title()}
       </h1>
