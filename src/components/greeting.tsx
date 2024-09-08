cd389ca6ed47dafefc091fc927248f9211a5398b
glyyynn <glynnjordan.jr@gmail.com>
2024-09-07 19:43:08 -0500

    prettier for change

--- src/components/greeting.tsx
+++ src/components/greeting.tsx
@@ -2,20 +2,20 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/auth-options';
 
 export const Greeting = async () => {
-    const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-    if (!session) {
-        return (
-          <div>
-            <h2>Welcome, Guest!</h2>
-            <p>Please sign in to access more features.</p>
-          </div>
-        );
-    }
-
+  if (!session) {
     return (
-        <div>
-            <h1>Welcome back {session.user?.name}!</h1>
-        </div>
-    )
-}
+      <div>
+        <h2>Welcome, Guest!</h2>
+        <p>Please sign in to access more features.</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Welcome back {session.user?.name}!</h1>
+    </div>
+  );
+};
